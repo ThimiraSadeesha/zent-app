58fa529243d59124d7a36a5a031d46c312ddc45e
Thimira Sadeesha <thimira@maximimpressions.com>
2026-02-04 09:21:14 +0530

    Remove legacy environment configurations, dashboard component, and related files for cleanup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import {BackgroundBeamsDemo} from "@/app/background";
+
+export default function Home() {
+    return (
+        <main>
+            <BackgroundBeamsDemo />
+        </main>
+    );
+}
--- next.config.ts
+++ next.config.ts
@@ -1,47 +1,10 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
-  allowedDevOrigins: [
-    "http://192.168.1.4:3000",
-    "local-origin.dev",
-    "*.local-origin.dev"
-  ],
-  turbopack: {
-    root: __dirname,
-    resolveAlias: {
-      "@/environments/environment": (() => {
-        const env =
-            process.env.NEXT_PUBLIC_ENV || process.env.NODE_ENV || "development";
-        const environmentFile =
-            env === "production"
-                ? "./src/environments/environment.production.ts"
-                : env === "qa"
-                    ? "./src/environments/environment.qa.ts"
-                    : "./src/environments/environment.development.ts";
-        return environmentFile;
-      })(),
+    turbopack: {
+        root: path.resolve(__dirname),
     },
-  },
-
-  webpack: (config) => {
-    const env =
-        process.env.NEXT_PUBLIC_ENV || process.env.NODE_ENV || "development";
-    const environmentFile =
-        env === "production"
-            ? "environment.production.ts"
-            : env === "qa"
-                ? "environment.qa.ts"
-                : "environment.development.ts";
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@/environments/environment$": require("path").resolve(
-          __dirname,
-          `src/environments/${environmentFile}`
-      ),
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
