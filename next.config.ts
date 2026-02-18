2c14b68c09f777c69ea32620f7c81435ee62411d
Thimira Sadeesha <thimira@maximimpressions.com>
2026-02-18 14:25:30 +0530

    pipeline test

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,11 @@ import path from "path";
 
 const nextConfig: NextConfig = {
     output: "standalone",
+
     allowedDevOrigins: ["192.168.23.207"],
+
     serverExternalPackages: ["ssh2"],
+
     turbopack: {
         root: path.resolve(__dirname),
     },
